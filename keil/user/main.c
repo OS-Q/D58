b2694ab71a856fea66afaebab0be54eba9bffe0d
qitas <qitas@qitas.cn>
2018-11-12 17:36:40 +0800

    add keil

---
+++ keil/user/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <intrins.h> 
+
+
+#include "CH552.h"
+#include "common.h"
+#include "Timer.h"
+#include "USB.h"
+#include "GPIO.h"
+
+
+
+ 
+void main()
+{ 	
+
+	while(1)
+	{
+
+    }
+}
+
+
+
+
